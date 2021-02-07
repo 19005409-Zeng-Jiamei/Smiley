036b2b35396486f110423e0b921312b54768ccda
Jiamei <19005409@DESKTOP-DMV0GKL>
2021-02-08 00:23:02 +0800

    :<  T^T :<

---
+++ Smiley/Controllers/DoorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Smiley.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Rendering; //for SelectListItem
+
+namespace Smiley.Controllers
+{
+    public class DoorController : Controller
+    {
+
+        [Authorize(Roles = "owner, admin")]
+        public IActionResult ViewDoors()
+        {
+            DataTable dt = DBUtl.GetTable("SELECT * FROM Door");
+
+            return View("ViewDoors", dt.Rows);
+
+        }
+
+    }
+}
--- Smiley/DBSetup.sql
+++ Smiley/DBSetup.sql
@@ -17,8 +17,7 @@ DROP TABLE IF EXISTS Feedback;
 --
 CREATE TABLE FaceId(
 	face_id             INT PRIMARY KEY IDENTITY,
-	face_picfile        VARCHAR(100)  NOT NULL,
-	face_email	    VARCHAR(100)  NOT NULL
+	face_picfile        VARCHAR(100)  NOT NULL
 );
 
 
@@ -26,31 +25,31 @@ CREATE TABLE FaceId(
 --
 --Insert records of user/customer face id
 SET IDENTITY_INSERT FaceId ON;
-INSERT INTO FaceId(face_id, face_picfile, face_email) VALUES
-(1, 'wwwroot/smileyUsers/Angeline', '19005146@myrp.edu.sg'),
-(2, 'wwwroot/smileyUsers/Janice', 'jiameizeng@gmail.com'),
-(3, 'wwwroot/smileyUsers/Jinhan', '19034471@myrp.edu.sg'),
-(4, 'wwwroot/customers/Customer1', 'nanalim@gmail.com'),
-(5, 'wwwroot/customers/Customer2', 'akaitan@gmail.com'),
-(6, 'wwwroot/customers/Customer3', 'aldouschen@gmail.com'),
-(7, 'wwwroot/customers/Customer4', 'alicemok@gmail.com'),
-(8, 'wwwroot/customers/Customer5', 'misayolim@gmail.com'),
-(9, 'wwwroot/customers/Customer6', 'alucardzeng@gmail.com'),
-(10, 'wwwroot/customers/Customer7', 'miyuutan@gmail.com'),
-(11, 'wwwroot/customers/Customer8', 'angelalee@gmail.com'),
-(12, 'wwwroot/customers/Customer9', 'aurorachen@gmail.com'),
-(13, 'wwwroot/customers/Customer10', 'marukomok@gmail.com'),
-(14, 'wwwroot/customers/Customer11', 'azezeng@gmail.com'),
-(15, 'wwwroot/customers/Customer12', 'brodylee@gmail.com'),
-(16, 'wwwroot/customers/Customer13', 'beatrixlee@gmail.com'),
-(17, 'wwwroot/customers/Customer14', 'claudetan@gmail.com'),
-(18, 'wwwroot/customers/Customer15', 'atomke@gmail.com'),
-(19, 'wwwroot/customers/Customer16', 'chimok@gmail.com'),
-(20, 'wwwroot/customers/Customer17', 'alphachen@gmail.com'),
-(21, 'wwwroot/customers/Customer18', 'lunamok@gmail.com'),
-(22, 'wwwroot/customers/Customer19', 'lunoxke@gmail.com'),
-(23, 'wwwroot/customers/Customer20', 'grangerzeng@gmail.com'),
-(24, 'wwwroot/smileyUsers/Jiamei', '19005409@myrp.edu.sg')
+INSERT INTO FaceId(face_id, face_picfile) VALUES
+(1,'19005146@myrp.edu.sg'), ---name face_picfile as email address to prevent duplicate
+(2,'jiameizeng@gmail.com'),
+(3,'19034471@myrp.edu.sg'),
+(4,'nanalim@gmail.com'),
+(5,'akaitan@gmail.com'),
+(6,'aldouschen@gmail.com'),
+(7,'alicemok@gmail.com'),
+(8,'misayolim@gmail.com'),
+(9,'alucardzeng@gmail.com'),
+(10,'miyuutan@gmail.com'),
+(11,'angelalee@gmail.com'),
+(12,'aurorachen@gmail.com'),
+(13,'marukomok@gmail.com'),
+(14,'azezeng@gmail.com'),
+(15,'brodylee@gmail.com'),
+(16,'beatrixlee@gmail.com'),
+(17,'claudetan@gmail.com'),
+(18,'atomke@gmail.com'),
+(19,'chimok@gmail.com'),
+(20,'alphachen@gmail.com'),
+(21,'lunamok@gmail.com'),
+(22,'lunoxke@gmail.com'),
+(23,'grangerzeng@gmail.com'),
+(24,'19005409@myrp.edu.sg')
 SET IDENTITY_INSERT FaceId OFF;
 
 
@@ -65,7 +64,7 @@ CREATE TABLE SmileyCustomer(
 	email			VARCHAR(50)  	NOT NULL,
 	membership          	VARCHAR(50)  	NOT NULL,
 	picfile                 VARCHAR(200)    NULL,
-	signup_date         	DATE   	 	NOT NULL,
+	signup_date         	DATETIME   	NOT NULL,
 	face_id			INT 		NOT NULL,
 	CONSTRAINT fk1 FOREIGN KEY(face_id) REFERENCES FaceId(face_id)
 );
@@ -76,26 +75,26 @@ CREATE TABLE SmileyCustomer(
 --
 SET IDENTITY_INSERT SmileyCustomer ON;
 INSERT INTO SmileyCustomer(customer_id, customer_name, surname, email, membership, signup_date, face_id) VALUES
-(00001, 'Nana', 'Lim', 'nanalim@gmail.com', 'bronze', '2020-01-21', 4),
-(00002, 'Akai', 'Tan', 'akaitan@gmail.com', 'gold','2020-01-21', 5),
-(00003, 'Aldous', 'Chen','aldouschen@gmail.com', 'silver', '2020-01-21', 6),
-(00004, 'Alice', 'Mok','alicemok@gmail.com', 'silver', '2020-01-21', 7),
-(00005, 'Misayo', 'Lim','misayolim@gmail.com', 'bronze', '2020-03-03', 8),
-(00006, 'Alucard', 'Zeng','alucardzeng@gmail.com', 'bronze',  '2020-03-03', 9),
-(00007, 'Miyuu', 'Tan','miyuutan@gmail.com', 'bronze', '2020-03-03', 10),
-(00008, 'Angela', 'Lee','angelalee@gmail.com', 'silver', '2020-03-03', 11),
-(00009, 'Aurora', 'Chen','aurorachen@gmail.com', 'silver', '2020-04-28', 12),
-(00010, 'Maruko', 'Mok','marukomok@gmail.com', 'gold', '2020-04-28', 13),
-(00011, 'Aze', 'Zeng','azezeng@gmail.com', 'gold', '2020-04-28', 14),
-(00012, 'Brody', 'Lee','brodylee@gmail.com', 'gold',  '2021-01-01', 15),
-(00013, 'Beatrix', 'Zeng','beatrixlee@gmail.com', 'silver', '2021-01-21', 16),
-(00014, 'Claude', 'Tan','claudetan@gmail.com', 'gold', '2021-01-21', 17),
-(00015, 'Atom', 'ke','atomke@gmail.com', 'silver', '2021-01-21', 18),
-(00016, 'Chi', 'Mok','chimok@gmail.com', 'bronze', '2021-01-28', 19),
-(00017, 'Alpha', 'Chen','alphachen@gmail.com', 'bronze', '2021-01-28', 20),
-(00018, 'Luna', 'Mok','lunamok@gmail.com', 'bronze', '2021-01-28', 21),
-(00019, 'Lunox', 'ke','lunoxke@gmail.com', 'silver', '2021-01-28', 22),
-(00020, 'Granger', 'Zeng','grangerzeng@gmail.com', 'silver', '2021-01-28', 23);
+(00001, 'Nana', 'Lim', 'nanalim@gmail.com', 'bronze', '2020-01-21 08:01:33', 4),
+(00002, 'Akai', 'Tan', 'akaitan@gmail.com', 'gold','2020-01-21 08:03:34', 5),
+(00003, 'Aldous', 'Chen','aldouschen@gmail.com', 'silver', '2020-01-21 09:01:33', 6),
+(00004, 'Alice', 'Mok','alicemok@gmail.com', 'silver', '2020-01-21 09:05:33', 7),
+(00005, 'Misayo', 'Lim','misayolim@gmail.com', 'bronze', '2020-03-03 10:01:33', 8),
+(00006, 'Alucard', 'Zeng','alucardzeng@gmail.com', 'bronze',  '2020-03-03 11:01:33', 9),
+(00007, 'Miyuu', 'Tan','miyuutan@gmail.com', 'bronze', '2020-03-03 12:01:33', 10),
+(00008, 'Angela', 'Lee','angelalee@gmail.com', 'silver', '2020-03-03 13:01:33', 11),
+(00009, 'Aurora', 'Chen','aurorachen@gmail.com', 'silver', '2020-04-28 14:01:33', 12),
+(00010, 'Maruko', 'Mok','marukomok@gmail.com', 'gold', '2020-04-28 16:01:33', 13),
+(00011, 'Aze', 'Zeng','azezeng@gmail.com', 'gold', '2020-04-28 16:01:33', 14),
+(00012, 'Brody', 'Lee','brodylee@gmail.com', 'gold',  '2021-01-01 17:01:33', 15),
+(00013, 'Beatrix', 'Zeng','beatrixlee@gmail.com', 'silver', '2021-01-21 18:01:33', 16),
+(00014, 'Claude', 'Tan','claudetan@gmail.com', 'gold', '2021-01-21 18:01:33', 17),
+(00015, 'Atom', 'ke','atomke@gmail.com', 'silver', '2021-01-21 19:01:33', 18),
+(00016, 'Chi', 'Mok','chimok@gmail.com', 'bronze', '2021-01-28 19:01:33', 19),
+(00017, 'Alpha', 'Chen','alphachen@gmail.com', 'bronze', '2021-01-28 20:01:33', 20),
+(00018, 'Luna', 'Mok','lunamok@gmail.com', 'bronze', '2021-01-28 20:01:33', 21),
+(00019, 'Lunox', 'ke','lunoxke@gmail.com', 'silver', '2021-01-28 21:01:33', 22),
+(00020, 'Granger', 'Zeng','grangerzeng@gmail.com', 'silver', '2021-01-28 22:01:33', 23);
 SET IDENTITY_INSERT SmileyCustomer OFF;
 
 
@@ -107,7 +106,6 @@ CREATE TABLE SmileyUser(
 	smiley_user_id		VARCHAR(20)   	PRIMARY KEY,
 	smiley_user_pw      	VARBINARY(50) 	NOT NULL,
 	full_name		VARCHAR(50)   	NOT NULL,
-	smiley_user_picfile	VARCHAR(200)  	NOT NULL,
 	email			VARCHAR(50)   	NOT NULL,
 	smiley_user_role    	VARCHAR(20)   	NOT NULL,
 	last_login          	DATETIME      	NULL,
@@ -121,12 +119,12 @@ CREATE TABLE SmileyUser(
 --
 --Insert smiley users' records
 --
-INSERT INTO SmileyUser (smiley_user_id, smiley_user_pw , full_name, smiley_user_picfile, email, smiley_user_role, superior_id, face_id) VALUES 
-('janice', HASHBYTES('SHA1', 'adminpw1'), 'Janice Chang','wwwroot/smileyUsers/Janice','jiameizeng@gmail.com','admin', NULL, 2), --admin can CRUD all available sensor records
-('angeline',    HASHBYTES('SHA1', 'password2'), 'Angeline Mok', 'wwwroot/smileyUsers/Angeline','19005146@myrp.edu.sg','owner', NULL, 1),
-('jinhan',   HASHBYTES('SHA1', 'password3'), 'Jinhan Ke','wwwroot/smileyUsers/Jinhan' ,'19034471@myrp.edu.sg', 'owner', NULL, 3),
+INSERT INTO SmileyUser (smiley_user_id, smiley_user_pw , full_name, email, smiley_user_role, superior_id, face_id) VALUES 
+('janice', HASHBYTES('SHA1', 'adminpw1'), 'Janice Chang','jiameizeng@gmail.com','admin', NULL, 2), --admin can CRUD all available sensor records
+('angeline',    HASHBYTES('SHA1', 'password2'), 'Angeline Mok','19005146@myrp.edu.sg','owner', NULL, 1),
+('jinhan',   HASHBYTES('SHA1', 'password3'), 'Jinhan Ke','19034471@myrp.edu.sg', 'owner', NULL, 3),
 --owner can CRUD all of its sensor records
-('jiamei',   HASHBYTES('SHA1', 'password1'), 'Jiamei Zeng','wwwroot/smileyUsers/Jiamei', '19005409@myrp.edu.sg','user', 'jinhan',23 );--user can view all of its sensor records
+('jiamei',   HASHBYTES('SHA1', 'password1'), 'Jiamei Zeng','19005409@myrp.edu.sg','user', 'jinhan',23 );--user can view all of its sensor records
  
 
 --
@@ -193,8 +191,8 @@ SET IDENTITY_INSERT Exact_Location OFF;
 --
 CREATE TABLE Sensor(
 	sensor_id       INT PRIMARY KEY IDENTITY,
-	start_time      TIME  	     NULL,
-	end_time        TIME         NULL,
+	start_time      TIME         NOT NULL,
+	end_time        TIME         NOT NULL,
 	sensor_status	TINYINT      NOT NULL,
 	smiley_user_id  VARCHAR(20)  NOT NULL,
 	location_id     INT 	     NOT NULL,
@@ -206,17 +204,17 @@ CREATE TABLE Sensor(
 --Insert sensor record
 --
 SET IDENTITY_INSERT Sensor ON;
-INSERT INTO Sensor(sensor_id, sensor_status, smiley_user_id, location_id) VALUES
-(1, 1, 'jiamei', 1),
-(2, 0, 'jiamei', 2),
-(3, 1, 'jiamei', 3),
-(4, 0, 'angeline', 4),
-(5, 1, 'angeline', 5),
-(6, 0, 'angeline', 6),
-(7, 1, 'jinhan', 7),
-(8, 0, 'jinhan', 8),
-(9, 1, 'jinhan', 9),
-(10, 0, 'jinhan', 10);
+INSERT INTO Sensor(sensor_id, start_time, end_time, sensor_status, smiley_user_id, location_id) VALUES
+(1, '08:00', '20:00', 1, 'jiamei', 1),
+(2, '09:00', '21:00', 0, 'jiamei', 2),
+(3, '05:00', '20:00', 1, 'jiamei', 3),
+(4, '10:00', '22:00', 0, 'angeline', 4),
+(5, '11:00', '23:00', 1, 'angeline', 5),
+(6, '9:30', '21:30', 0, 'angeline', 6),
+(7, '10:30', '22:30', 1, 'jinhan', 7),
+(8, '08:30', '20:30', 0, 'jinhan', 8),
+(9, '05:00', '22:00', 1, 'jinhan', 9),
+(10, '11:00', '22:00', 0, 'jinhan', 10);
 SET IDENTITY_INSERT Sensor OFF;
 
 
@@ -226,8 +224,8 @@ SET IDENTITY_INSERT Sensor OFF;
 CREATE TABLE Emotion(
 	emotion_record_id      	INT PRIMARY KEY IDENTITY,
 	emotion_type		VARCHAR(50)   NOT NULL,
-	time_stamp       	DATE   NOT NULL,
-	sensor_id		INT NOT NULL,
+	time_stamp       	DATETIME      NOT NULL,
+	sensor_id		INT           NOT NULL,
 	CONSTRAINT fk6 FOREIGN KEY(sensor_id) REFERENCES Sensor(sensor_id)
 );
 
@@ -237,26 +235,26 @@ CREATE TABLE Emotion(
 --
 SET IDENTITY_INSERT Emotion ON;
 INSERT INTO Emotion(emotion_record_id, emotion_type, time_stamp, sensor_id) VALUES
-(1, 'anger', '2020-01-21', 1),
-(2, 'anger', '2020-01-21', 1),
-(3, 'anticipation', '2020-01-21', 1),
-(4, 'anticipation', '2020-01-21', 1),
-(5, 'joy', '2020-01-21', 2),
-(6, 'joy', '2020-01-21', 2),
-(7, 'joy', '2020-01-21', 2),
-(8, 'trust', '2020-01-21', 3),
-(9, 'trust', '2020-01-21', 4),
-(10, 'trust', '2020-01-21', 4),
-(11, 'fear', '2020-01-21', 5),
-(12, 'fear', '2020-01-21', 5),
-(13, 'fear', '2020-01-21', 6),
-(14, 'surprise', '2020-01-21', 7),
-(15, 'surprise', '2020-01-21', 7),
-(16, 'surprise', '2020-01-21', 8),
-(17, 'sadness', '2020-01-21', 8),
-(18, 'disgust', '2020-01-21', 9),
-(19, 'digust', '2020-01-21', 9),
-(20, 'sadness', '2020-01-21', 10);
+(1, 'anger', '2020-01-21 09:00:01', 1),
+(2, 'anger', '2020-01-21 09:10:01', 1),
+(3, 'anticipation', '2020-01-21 09:11:01', 1),
+(4, 'anticipation', '2020-01-21 09:12:01', 1),
+(5, 'joy', '2020-01-21 10:01:21', 2),
+(6, 'joy', '2020-01-21 11:00:01', 2),
+(7, 'joy', '2020-01-21 11:22:01', 2),
+(8, 'trust', '2020-01-21 12:23:01', 3),
+(9, 'trust', '2020-01-21 13:11:01', 4),
+(10, 'trust', '2020-01-21 13:11:01', 4),
+(11, 'fear', '2020-01-21 14:12:01', 5),
+(12, 'fear', '2020-01-21 14:14:01', 5),
+(13, 'fear', '2020-01-21 15:11:01', 6),
+(14, 'surprise', '2020-01-21 15:12:01', 7),
+(15, 'surprise', '2020-01-21 16:13:01', 7),
+(16, 'surprise', '2020-01-21 16:14:01', 8),
+(17, 'sadness', '2020-01-21 17:11:01', 8),
+(18, 'disgust', '2020-01-21 17:11:01', 9),
+(19, 'digust', '2020-01-21 16:11:01', 9),
+(20, 'sadness', '2020-01-21 16:11:01', 10);
 SET IDENTITY_INSERT Emotion OFF;
 
 --
@@ -264,9 +262,9 @@ SET IDENTITY_INSERT Emotion OFF;
 --
 CREATE TABLE Door(
 	door_record_id   INT PRIMARY KEY IDENTITY,
-	door_gesture	 VARCHAR(50) NOT NULL,
-	time_stamp       DATE        NOT NULL,
-	sensor_id	 INT 	     NOT NULL,
+	door_gesture	 VARCHAR(50)     NOT NULL,
+	time_stamp       DATETIME        NOT NULL,
+	sensor_id	 INT 	         NOT NULL,
 	CONSTRAINT fk7 FOREIGN KEY(sensor_id) REFERENCES Sensor(sensor_id)
 );
 
@@ -275,26 +273,26 @@ CREATE TABLE Door(
 --
 SET IDENTITY_INSERT Door ON;
 INSERT INTO Door(door_record_id, door_gesture, time_stamp, sensor_id) VALUES
-(1, 'Open', '2020-01-21', 1),
-(2, 'Open', '2020-01-21',1),
-(3, 'Open', '2020-01-21',1),
-(4, 'Close', '2020-01-21',1),
-(5, 'Open', '2020-03-03',2),
-(6, 'Open', '2020-03-03',2),
-(7, 'Open', '2020-03-03',2),
-(8, 'Close', '2020-03-03',3),
-(9, 'Open', '2020-04-28',4),
-(10, 'Open', '2020-04-28',4),
-(11, 'Open', '2020-04-28',5),
-(12, 'Close', '2021-01-01',5),
-(13, 'Open', '2021-01-21',6),
-(14, 'Open', '2021-01-21',7),
-(15, 'Open', '2021-01-21',7),
-(16, 'Open', '2021-01-28',8),
-(17, 'Open', '2021-01-28',8),
-(18, 'Open', '2021-01-28',9),
-(19, 'Open', '2021-01-28',9),
-(20, 'Open', '2021-01-28',10);
+(1, 'Open', '2020-01-21 11:00:00', 1),
+(2, 'Open', '2020-01-21 11:11:11',1),
+(3, 'Open', '2020-01-21 11:12:12',1),
+(4, 'Close', '2020-01-21 12:10:01',1),
+(5, 'Open', '2020-03-03 12:05:07',2),
+(6, 'Open', '2020-03-03 13:00:01',2),
+(7, 'Open', '2020-03-03 14:00:01',2),
+(8, 'Close', '2020-03-03 14:05:07',3),
+(9, 'Open', '2020-04-28 15:11:08',4),
+(10, 'Open', '2020-04-28 15:14:07',4),
+(11, 'Open', '2020-04-28 12:05:07',5),
+(12, 'Close', '2021-01-01 12:05:07',5),
+(13, 'Open', '2021-01-21 13:11:05',6),
+(14, 'Open', '2021-01-21 13:10:06',7),
+(15, 'Open', '2021-01-21 14:05:02',7),
+(16, 'Open', '2021-01-28 14:05:01',8),
+(17, 'Open', '2021-01-28 15:20:11',8),
+(18, 'Open', '2021-01-28 15:20:50',9),
+(19, 'Open', '2021-01-28 16:30:40',9),
+(20, 'Open', '2021-01-28 16:30:33',10);
 SET IDENTITY_INSERT Door OFF;
 
 --
@@ -303,7 +301,7 @@ SET IDENTITY_INSERT Door OFF;
 CREATE TABLE Feedback(
 	feedback_id	  INT PRIMARY KEY IDENTITY,
 	feedback_gesture  VARCHAR(50) NOT NULL,		
-	time_stamp        DATE        NOT NULL,
+	time_stamp        DATETIME    NOT NULL,
 	sensor_id         INT 	      NOT NULL,
 	CONSTRAINT fk8 FOREIGN KEY(sensor_id) REFERENCES Sensor(sensor_id)
 );
@@ -313,26 +311,26 @@ CREATE TABLE Feedback(
 --
 SET IDENTITY_INSERT Feedback ON;
 INSERT INTO Feedback(feedback_id, feedback_gesture, time_stamp, sensor_id) VALUES
-(1, 'Good', '2020-01-21', 1),
-(2, 'Good', '2020-01-21',1),
-(3, 'Good', '2020-01-21',1),
-(4, 'Good', '2020-01-21',1),
-(5, 'Good', '2020-03-03',2),
-(6, 'Bad', '2020-03-03',2),
-(7, 'Bad', '2020-03-03',2),
-(8, 'Bad', '2020-03-03',3),
-(9, 'Bad', '2020-04-28',4),
-(10, 'Neutral', '2020-04-28',4),
-(11, 'Neutral', '2020-04-28',5),
-(12, 'Good', '2021-01-01',5),
-(13, 'Good', '2021-01-21',6),
-(14, 'Neutral', '2021-01-21',7),
-(15, 'Neutral', '2021-01-21',7),
-(16, 'Good', '2021-01-28',8),
-(17, 'Good', '2021-01-28',8),
-(18, 'Bad', '2021-01-28',9),
-(19, 'Bad', '2021-01-28',9),
-(20, 'Bad', '2021-01-28',10);
+(1, 'Good', '2020-01-21 11:12:11', 1),
+(2, 'Good', '2020-01-21 11:13:33',1),
+(3, 'Good', '2020-01-21 11:14:05',1),
+(4, 'Good', '2020-01-21 11:15:00',1),
+(5, 'Good', '2020-03-03 12:00:09',2),
+(6, 'Bad', '2020-03-03 12:00:11',2),
+(7, 'Bad', '2020-03-03 13:00:11',2),
+(8, 'Bad', '2020-03-03 13:00:11',3),
+(9, 'Bad', '2020-04-28 14:00:11',4),
+(10, 'Neutral', '2020-04-28 14:00:11',4),
+(11, 'Neutral', '2020-04-28 14:00:11',5),
+(12, 'Good', '2021-01-01 15:09:11',5),
+(13, 'Good', '2021-01-21 15:00:11',6),
+(14, 'Neutral', '2021-01-21 16:00:11',7),
+(15, 'Neutral', '2021-01-21 16:00:11',7),
+(16, 'Good', '2021-01-28 17:00:11',8),
+(17, 'Good', '2021-01-28 17:00:11',8),
+(18, 'Bad', '2021-01-28 19:00:11',9),
+(19, 'Bad', '2021-01-28 11:00:11',9),
+(20, 'Bad', '2021-01-28 12:00:11',10);
 SET IDENTITY_INSERT Feedback OFF;
 
 
